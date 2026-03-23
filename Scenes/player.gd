42d69fc348bbc2100bb3ebdf6c7cb1a1fb84e528
Naomie-Tuyishime <tuyishimenaome58@gmail.com>
2026-03-23 17:51:53 +0200

    ui functionality

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -15,36 +15,36 @@ var is_interior_view = false
 @onready var interior_cam = $InternalCamera
 
 func _ready() -> void:
-	exterior_cam.current = true
-	interior_cam.current = false
+    exterior_cam.current = true
+    interior_cam.current = false
 func _input(event):
-	if Input.is_action_just_pressed("toggle_camera"):
-		is_interior_view = !is_interior_view
-		exterior_cam.current = !is_interior_view
-		interior_cam.current = is_interior_view
+    if Input.is_action_just_pressed("toggle_camera"):
+        is_interior_view = !is_interior_view
+        exterior_cam.current = !is_interior_view
+        interior_cam.current = is_interior_view
 func _physics_process(delta: float) -> void:
-	var throttle = Input.get_axis("move_backward", "move_forward")
-	var steer = Input.get_axis("steer_right", "steer_left")
-	
-	current_steering = lerp(current_steering, steer * max_steering, delta * 5.0)
-	
-	var speed = linear_velocity.length()
-	if speed > 0.5:
-		rotate_y(current_steering * 4 * delta)
-	
-	var forward = -global_transform.basis.z * throttle
-	apply_central_force(forward * max_engine_force * delta)
-	
-	var spin_speed = speed * delta * 3.0
+    var throttle = Input.get_axis("move_backward", "move_forward")
+    var steer = Input.get_axis("steer_right", "steer_left")
+    
+    current_steering = lerp(current_steering, steer * max_steering, delta * 5.0)
+    
+    var speed = linear_velocity.length()
+    if speed > 0.5:
+        rotate_y(current_steering * 4 * delta)
+    
+    var forward = -global_transform.basis.z * throttle
+    apply_central_force(forward * max_engine_force * delta)
+    
+    var spin_speed = speed * delta * 3.0
 
-	if throttle != 0 and speed > 0.5:
-		if throttle > 0:
-			wheel_spin -= spin_speed * 60 
-		else:
-			wheel_spin += spin_speed * 60
-	
-	wheel_fl.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
-	wheel_fr.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
-	wheel_rl.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
-	wheel_rr.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
-	#body.rotation = Vector3(deg_to_rad(270), current_steering*2, 0)
+    if throttle != 0 and speed > 0.5:
+        if throttle > 0:
+            wheel_spin -= spin_speed * 60 
+        else:
+            wheel_spin += spin_speed * 60
+    
+    wheel_fl.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
+    wheel_fr.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
+    wheel_rl.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
+    wheel_rr.rotation = Vector3(deg_to_rad(wheel_spin), current_steering, 0)
+    #body.rotation = Vector3(deg_to_rad(270), current_steering*2, 0)
